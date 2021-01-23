0e4396e734d9d1e8733de586c4458b02af8ffa5f
ayeonee <zn_yeon@naver.com>
2021-01-23 19:13:42 +0900

    save userInfo & make onLogin function

--- login_test/src/App.js
+++ login_test/src/App.js
@@ -1,95 +1,26 @@
-import React, {useState} from 'react';
-import styled from "styled-components";
-import GoogleLogin from 'react-google-login';
-import KakaoLogin from 'react-kakao-login';
+import React, {useState} from "react";
+//import styled from 'styled-components';
+import Store from './Store/store';
+import LoginContainer from "./Components/LoginContainer";
 
-const kakaoToken = "d61079c156018c7a8055d9a015191dfa";
+const App = () =>{
+  const [logged, setLogged] = useState(false);
 
-
-const Login = () => {
-
-  const [userInfo, setUserInfo]=useState({
-    id:'',
-    name : '',
-    provider:'',
-  });
-
-  const {id, name, provider} = userInfo;
-
-  const responseGoogle = (response)=>{
-    setUserInfo({
-      [id] : response.googleId,
-      [name] : response.profileObj.name,
-      [provider] : "google",
-    });
+  const onLogin=()=>{
+    setLogged(true);
   }
 
-  const responseKakao = (response) => {
-    setUserInfo({
-      [id] : response.profile.id,
-      [name] : response.profile.properties.nickname,
-      [provider] : "kakao"
-    });
-  }
-  console.log(userInfo);
-
-  const responseFail=(err)=>{
-    console.error(err);
+  const onLogout=()=>{
+    setLogged(false);
   }
 
-  return(
-    <LoginContainer>
-      <Container>
-        <ContainerTop>
-          <h1>Welcome To MEEMO!</h1>
-        </ContainerTop>
-        <ContainerBody>
-          <GoogleLogin
-            clientId="723578906212-p9e6ejvqm6rbbk4d2lo0djvks5j3k530.apps.googleusercontent.com"
-            buttonBox = "GoogleLogin"
-            onSuccess={responseGoogle}
-            onFailure={responseFail}
-            cookiePolicy={'single_host_origin'}
-          />
-          <KakaoLogin
-            token={kakaoToken}
-            onSuccess={responseKakao}
-            onFail={responseFail}
-            onLogout={console.info}
-            useLoginForm
-          />
-        </ContainerBody>
-      </Container>
-    </LoginContainer>
+  return (
+   <div>
+     <Store.Provider value={logged, onLogin, onLogout}>
+       <LoginContainer/>
+     </Store.Provider>
+   </div>
   );
-  
 };
 
-const LoginContainer = styled.div`
-  width : 100vw;
-  height: 100vh;
-  display : flex;
-  justify-content : center;
-  align-items : center;
-`;
-
-const Container = styled.div`
-  border : 2px solid grey;
-  width: 600px;
-  height: 500px;
-`;
-
-const ContainerTop = styled.div`
-    background-color: white;
-    border: none;
-    text-align:center;
-`;
-
-const ContainerBody = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: flex-end;
-    height: 300px;
-`;
-
-export default Login;
+export default App;
---
+++ login_test/src/Components/Login.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import styled from "styled-components";
+import GoogleLogin from 'react-google-login';
+import KakaoLogin from 'react-kakao-login';
+
+const Login = ({onLogin}) => {
+  const [userInfo, setUserInfo]=useState([]);
+
+  const responseGoogle = (response)=>{
+    setUserInfo([
+      {
+        id:userInfo.length,
+        userId : response.googleId,
+        userName : response.profileObj.name,
+        provider : "google",
+      }
+    ]);
+    onLogin();
+  }
+
+  const responseKakao = (response) => {
+    setUserInfo([
+      {
+        id:userInfo.length,
+        userId : response.profile.id,
+        userName : response.profile.properties.nickname,
+        provider : "kakao"
+      }
+    ]);
+    onLogin();
+  }
+  console.log(userInfo);
+
+  const responseFail=(err)=>{
+    console.error(err);
+  }
+
+  return(
+    <LoginContainer>
+      <Container>
+        <ContainerTop>
+          <h1>Welcome To MEEMO!</h1>
+        </ContainerTop>
+        <ContainerBody>
+          <GoogleLogin
+            clientId="723578906212-p9e6ejvqm6rbbk4d2lo0djvks5j3k530.apps.googleusercontent.com"
+            buttonBox = "GoogleLogin"
+            onSuccess={responseGoogle}
+            onFailure={responseFail}
+            cookiePolicy={'single_host_origin'}
+          />
+          <KakaoLogin
+            token="d61079c156018c7a8055d9a015191dfa"
+            onSuccess={responseKakao}
+            onFail={responseFail}
+            onLogout={console.info}
+            useLoginForm
+          />
+        </ContainerBody>
+      </Container>
+    </LoginContainer>
+  );
+  
+};
+
+const LoginContainer = styled.div`
+  width : 100vw;
+  height: 100vh;
+  display : flex;
+  justify-content : center;
+  align-items : center;
+`;
+
+const Container = styled.div`
+  border : 2px solid grey;
+  width: 600px;
+  height: 500px;
+`;
+
+const ContainerTop = styled.div`
+    background-color: white;
+    border: none;
+    text-align:center;
+`;
+
+const ContainerBody = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    height: 300px;
+`;
+
+export default Login;
