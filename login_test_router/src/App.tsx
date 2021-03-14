fcb634bac1f4db4b5ea68174e760a78cf3147d06
ayeonee <zn_yeon@naver.com>
2021-03-14 16:29:31 +0900

    apply type

--- login_test_router/src/App.tsx
+++ login_test_router/src/App.tsx
@@ -4,15 +4,22 @@ import { signIn } from './Components/auth';
 import Login from './Components/Login';
 import MyPage from './Components/MyPage';
 
+interface User{
+  userId : string,
+  userName : string,
+  provider : string
+}
+
 const App : React.FC =() => {
-  const [user, setUser]=useState({
+  const [user, setUser]=useState<User>({
     userId : "",
     userName:"",
     provider:""
   });
-  const authenticated = user != null;
 
-  const login=(userId : any)=>setUser(signIn(userId));
+  const authenticated = user != null; //인증여부 저장
+
+  const login=(userId : string)=>setUser(signIn(userId));
   const logout = () => setUser({
     userId : "",
     userName : "",
--- login_test_router/src/Components/auth.tsx
+++ login_test_router/src/Components/auth.tsx
@@ -1,4 +1,10 @@
-const users=[
+interface User{
+    userId : string,
+    userName : string,
+    provider : string
+  }
+
+const users : User[] =[
     {
         userId : "115066686759720403740",
         userName : "김아연",
@@ -11,7 +17,7 @@ const users=[
     }
 ]
 
-export function signIn(userId : any){
+export function signIn(userId : string){
     const user = users.find((user) => user.userId === userId)
     if(user===undefined) throw new Error()
     return user;
