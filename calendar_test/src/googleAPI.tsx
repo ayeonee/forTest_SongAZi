17721f897a1bbdcf4677178569bcf7f0566e12c6
ayeonee <zn_yeon@naver.com>
2021-04-07 23:43:59 +0900

    try to add googleCalendarAPI

---
+++ calendar_test/src/googleAPI.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode, SyntheticEvent } from 'react';
+import apiCalendar from 'react-google-calendar-api/src/ApiCalendar';
+
+export default class DoubleButton extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  public handleItemClick(event: SyntheticEvent<any>, name: string): void {
+    if (name === 'sign-in') {
+      apiCalendar.handleAuthClick();
+    } else if (name === 'sign-out') {
+      apiCalendar.handleSignoutClick();
+    }
+  }
+
+  render(): ReactNode {
+    return (
+      <>
+        <button onClick={(e) => this.handleItemClick(e, 'sign-in')}>sign-in</button>
+        <button onClick={(e) => this.handleItemClick(e, 'sign-out')}>sign-out</button>
+      </>
+    );
+  }
+}
